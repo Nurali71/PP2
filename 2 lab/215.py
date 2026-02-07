19e35eb0c3176d6e7ca1501c71f5ae9563ff3283
Nurali71 <nurali.talgat08@gmail.com>
2026-02-07 13:56:51 +0500

    Practice 2

--- 2 lab/215.py
+++ 2 lab/215.py
@@ -1,2 +1,8 @@
-a=int(input())
-b=[]
+n = int(input())
+surnames = set()
+
+for _ in range(n):
+    surname = input().strip()
+    surnames.add(surname)
+
+print(len(surnames))
---
+++ Practice2/Boolean/Booleans as Comparison Results.py
@@ -0,0 +1,9 @@
+# Booleans as comparison results
+
+a = 10
+b = 5
+
+print(a > b)    # True
+print(a < b)    # False
+print(a == b)   # False
+print(a != b)   # True
