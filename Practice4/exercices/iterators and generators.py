fc076778fb634cc07c6291b2ff3cbb67401d7058
Nurali71 <nurali.talgat08@gmail.com>
2026-02-27 17:19:13 +0500

    Practice4-exercices-gen and iter

---
+++ Practice4/exercices/iterators and generators.py
@@ -0,0 +1,37 @@
+# 1 Create a generator that generates the squares of numbers up to some number N.
+
+def square_generatot(n):
+    for i in range(1, 1+n):
+        yield i**2
+a=int(input())
+squares=square_generatot(a)
+for result in squares:
+    print(result)
+# 2
+def even_generator(n):
+    for i in range(0, n+1):
+        if i%2==0:
+            yield str(i)
+n=int(input())
+gen=even_generator(n)
+result=",".join(gen)
+print(result)
+# 3
+def my_generator(n):
+    for i in range(0,n+1):
+        if i%3==0 and i%4==0:
+            yield str(i)
+n=int(input())
+gen=my_generator(n)
+result=" ".join(gen)
+print(result)
+# 4
+def squares(a, b):
+    for i in range(a, b+1):
+        yield i**2
+a, b=map(int, input().split())
+gen=squares(a, b)
+for res in gen:
+    print(res)
+
+
