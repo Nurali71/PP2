011b25d64d4e2926ab54c5fc139adebd4d362c94
Nurali71 <nurali.talgat08@gmail.com>
2026-03-06 15:53:53 +0500

    Regular expression

---
+++ Practice5/RegEx/Match Object.py
@@ -0,0 +1,29 @@
+# A Match Object is an object containing information about the search and the result.
+
+# Do a search that will return a Match Object:
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+# .span() returns a tuple containing the start-, and end positions of the match.
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+# .string returns the string passed into the function
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+
+# .group() returns the part of the string where there was a match
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
---
+++ Practice5/RegEx/The split() Function.py
@@ -0,0 +1,7 @@
+# The split() function returns a list where the string has been split at each match:
+
+import re
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
