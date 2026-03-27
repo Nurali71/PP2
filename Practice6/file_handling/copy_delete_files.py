65cba7d3cde73affbda372e4b4837b538a57c008
Nurali71 <nurali.talgat08@gmail.com>
2026-03-27 22:57:01 +0500

    Add Practice 6: file handling, directories and builtins

---
+++ Practice6/file_handling/copy_delete_files.py
@@ -0,0 +1,8 @@
+# To delete a file, you must import the OS module, and run its os.remove() function:
+import os
+os.remove("demofile.txt") 
+# To delete an entire folder, use the os.rmdir() method:
+import os
+os.rmdir("myfolder") 
+
+
